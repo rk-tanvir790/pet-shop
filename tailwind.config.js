c7f155d9f3757cf459d9be4f3b28b9e8a3eda6c2
rk-tanvir790 <ktanvir790@gmail.com>
2025-01-31 23:08:15 +0600

    Project Environment setup & Resource Collected

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-btn-clr': '#FFDC26',  // custom button background color
+          'custom-section-bg': '#FFF9F4', // custom section background color
+          'custom-card-bg': '#F9DF8A', // custom card background color
+          'custom-text-clr': '#111111', // custom text color
+        },
+      },
+    },
+  }
+  
